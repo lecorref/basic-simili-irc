ceaef7cdf38cb0b658a56caa42eee2cda249ab24
francois LE CORRE <lecorre.f@gmail.com>
2016-09-19 17:11:00 +0200

    CHANGES: compiled linked list function in a separate static lib

--- serveur/src/error.c
+++ serveur/src/error.c
@@ -3,11 +3,11 @@
 
 void        usage(char *name)
 {
-    dprintf(2, "usage: %s port", name);
+    dprintf(2, "usage: %s port\n", name);
 }
 
 int         r_error(char *error)
 {
-    dprintf(2, "%s", error);
+    dprintf(2, "%s\n", error);
     return (1);
 }
